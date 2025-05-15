b44201796b5f0acd5064d202926f630825ed5472
U-Jayadhar <ujayadhar8.dev@gmail.com>
2025-05-15 23:39:27 +0530

    fetching data from firestore db
    Signed-off-by: U-Jayadhar <ujayadhar8.dev@gmail.com>

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -9,23 +9,44 @@
 
 import express, { json } from "express";
 import cors from "cors";
-// import { onRequest } from "firebase-functions/v1/https";
+import { initializeApp } from "firebase-admin/app";
+import { getFirestore } from "firebase-admin/firestore";
+import { onRequest } from "firebase-functions/v1/https";
 
 const app = express();
 app.use(json());
 app.use(cors({ origin: true }));
 
+initializeApp();
+const db = getFirestore();
+
 app.get("/", (req: any, res: any) => {
-  res.send("Hello from Firebase!");
+  res.send("Welcome to the PulseTab learning space!!");
 });
 
-
-const PORT = 3001;
-app.listen(PORT, () => {
-  console.log(
-    `Express server running successfully and listening on http://localhost:${PORT}/`
-  );
+app.get("/users", async (req: any, res: any) => {
+  try {
+    const userRef = db.collection("users");
+    const snapshot = await userRef.get();
+    const users: any[] = [];
+    snapshot.docs.forEach((doc) => {
+      users.push({
+        ...doc.data(),
+        id: doc.id,
+      });
+    });
+    res.status(200).json(users);
+  } catch (error) {
+    console.error("Error fetching users:", error);
+    res.status(500).json({ error: "Failed to fetch users" });
+  }
 });
 
+// const PORT = 3001;
+// app.listen(PORT, () => {
+//   console.log(
+//     `Express server running successfully and listening on http://localhost:${PORT}/`
+//   );
+// });
 
-// exports.webApi = onRequest(app);
+exports.webApi = onRequest(app);
